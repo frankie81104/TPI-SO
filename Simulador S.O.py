1ea5519e7b6769850d2bfe3efdde95a7992c60c5
RaulCollazo <195011609+RaulCollazo@users.noreply.github.com>
2025-11-02 17:13:08 -0300

    Creado csv - creada funcion lectura de archivo

--- Simulador S.O.py
+++ Simulador S.O.py
@@ -1,5 +1,8 @@
 from dataclasses import dataclass, field
 from typing import List
+import os
+
+#Definición de una clase partición
 
 @dataclass
 class Particion:
@@ -10,29 +13,41 @@ class Particion:
     fragmentacion: int
     ocupado: bool
 
+#Definciion de una clase proceso
+
 @dataclass
 class Proceso:
+    id_proceso: int
     nombre: str
     tamano: int
     arribo: int
     irrupcion: int
     t_memoria: int
 
+#Definición del arreglo de particiones
+#Cuando llegue un proceso en fragmentacion=espacio-tamaño del proceso
+
 arreglo_particiones: List[Particion] = [
-    Particion(id="Sistema Operativo", dir=0, espacio=100, id_proceso="", fragmentacion=0, ocupado=False), #cuando llegue un proceso en fragmentacion=espacio-tamaño del proceso
+    Particion(id="Sistema Operativo", dir=0, espacio=100, id_proceso="", fragmentacion=0, ocupado=True), 
     Particion(id="Trabajos Grandes", dir=100, espacio=250, id_proceso="", fragmentacion=0, ocupado=False),
     Particion(id="Trabajos Medianos", dir=351, espacio=150, id_proceso="", fragmentacion=0, ocupado=False),
     Particion(id="Trabajos Pequeños", dir=501, espacio=50, id_proceso="", fragmentacion=0, ocupado=False)
 ]
-#La idea es que los procesos ingresen por default a la cola de procesos, cuando se cumpla su arribo pasen a la cola de listo y se evaluen en base al metodo BestFit o el Algoritmo SRTF, se considera finalizado si t_memoria == irrupcion.
+#La idea es que los procesos ingresen por default a la cola de procesos,
+#cuando se cumpla su arribo pasen a la cola de listo y se evaluen en base al metodo BestFit o el Algoritmo SRTF, se considera finalizado si t_memoria == irrupcion.
 
 ColaProcesos=[]
 ColaListo=[]
 ColaSuspendido=[]
 ColaFinalizado=[]
 
-Procesos=0
+Procesos = 0
 
-def cargar_procesos(proceso,c_procesos):
-    while c_procesos < 11:
-        ColaProcesos.append(proceso)
+#Funcion encargada de cargar el contenido del archivo en la variable procesos
+def cargar_lista_procesos():
+    ruta = os.path.join(os.path.dirname(os.path.abspath(__file__)),"procesos.csv")
+    with open(ruta, "r", encoding="utf-8") as f:
+        procesos = f.read()
+    print(str(procesos))
+
+cargar_lista_procesos()
